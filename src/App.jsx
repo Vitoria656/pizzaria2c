cf57d208a807a343aed3677c44dc9ceda6562637
vitoria656 <vjordana101@gmail.com>
2024-06-13 09:56:23 -0300

    Alteração

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import React, {useState} from 'react'
 import {Routes, Route} from 'react-router-dom'
 import Home from './pages/Home'
-
+ 
 const App = () => {
-const [color] = useState("#a54fcc")
-document.body.style.backgroundColor = color
-
-return(
-  <Routes>
-    <Route path="/" element={<Home />} />
-  </Routes>
-)
+  const [color] = useState("#a54fcc")
+  document.body.style.backgroundColor = color
+ 
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  )
 }
-
+ 
 export default App
